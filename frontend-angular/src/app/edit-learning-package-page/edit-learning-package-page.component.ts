66f40fa01c11169eb648f74443f29e99f9d862f4
viviendbk <vivien.deblock@edu.devinci.fr>
2023-12-24 08:27:38 +0100

    a

---
+++ frontend-angular/src/app/edit-learning-package-page/edit-learning-package-page.component.ts
@@ -0,0 +1,56 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {LearningPackageService} from "../services/learning-package.service";
+import {LearningFactService} from "../services/learning-fact.service";
+
+interface LearningPackage {
+  learningPackageId: number
+  title: string
+  description: string
+}
+interface LearningFact {
+  learningFactId: number
+  title: string
+  question: string
+  answer: string
+  nextStudyTime: string
+  learningPackageId: number
+}
+
+@Component({
+  selector: 'app-edit-learning-package-page',
+  templateUrl: './edit-learning-package-page.component.html',
+  styleUrl: './edit-learning-package-page.component.css'
+})
+export class EditLearningPackagePageComponent implements OnInit{
+
+  learningFacts: LearningFact[] = [];
+  learningPackages: LearningPackage[] = [];
+  id = this.route.snapshot.paramMap.get('id');
+
+  constructor(
+    private route: ActivatedRoute,
+    private learningPackageService: LearningPackageService,
+    private learningFactService: LearningFactService
+  ) { }
+
+  ngOnInit(): void {
+    this.fetchLearningPackages();
+  }
+
+  fetchLearningPackages(): void {
+    this.learningPackageService.getLearningPackages().subscribe(
+      (data: any) => {
+        console.log('API Response:', data);
+        this.learningPackages = data; // Assuming your API returns an array of learning packages
+      },
+      error => {
+        console.error('Error fetching learning packages:', error);
+      }
+    );
+  }
+
+  navigateToLearningPackage(learningPackageId: any) {
+
+  }
+}
