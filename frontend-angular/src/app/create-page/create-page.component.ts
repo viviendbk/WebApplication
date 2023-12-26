1a9531e871933281d28257d85e62d9b194af973b
viviendbk <vivien.deblock@edu.devinci.fr>
2023-12-26 22:45:41 +0100

    adding create page + many more things

---
+++ frontend-angular/src/app/create-page/create-page.component.ts
@@ -0,0 +1,90 @@
+// learning-package-create.component.ts
+
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LearningFact, LearningPackage } from "../app.component";
+import { LearningPackageService } from '../services/learning-package.service';
+import { LearningFactService } from  '../services/learning-fact.service';
+@Component({
+  selector: 'app-create-page',
+  templateUrl: './create-page.component.html',
+  styleUrls: ['./create-page.component.css'],
+})
+export class CreatePageComponent {
+  learningPackageForm: FormGroup;
+  learningFactsFormArray: FormGroup[] = [];
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private learningPackageService: LearningPackageService,
+    private learningFactsService: LearningFactService
+  ) {
+    this.learningPackageForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      category: ['', Validators.required],
+    });
+
+    // Initially, add one form for creating LearningFact
+    this.addLearningFactForm();
+  }
+
+  createLearningPackage(): void {
+
+    if (this.learningPackageForm.valid) {
+      const learningPackage: LearningPackage = this.learningPackageForm.value;
+
+      // Create Learning Package
+      this.learningPackageService.createLearningPackage(learningPackage).subscribe(
+        (response) => {
+          console.log('Learning Package created:', response);
+
+          // Create Learning Facts
+          this.createLearningFacts(response.learningPackageId);
+        },
+        (error) => {
+          console.error('Error creating Learning Package:', error);
+        }
+      );
+    }
+  }
+
+  createLearningFacts(learningPackageId: number): void {
+    for (let learningFactForm of this.learningFactsFormArray) {
+      if (learningFactForm.valid) {
+        let learningFact: LearningFact = learningFactForm.value;
+        // Assign learningPackageId to the learningFact
+        learningFact.learningPackageId = learningPackageId;
+
+        // Create Learning Fact
+        this.learningFactsService.createLearningFact(learningFact).subscribe(
+          (response) => {
+            console.log('Learning Fact created:', response);
+            // Optionally, you can reset the form or update the UI
+          },
+          (error) => {
+            console.error('Error creating Learning Fact:', error);
+          }
+        );
+      }
+    }
+  }
+
+  addLearningFactForm(): void {
+    const learningFactForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      question: ['', Validators.required],
+      answer: ['', Validators.required],
+      nextStudyTime: new Date(),
+      lastDateReview: new Date(),
+      nbTimeReviewed: 0,
+      confidenceLevel: 0,
+    });
+
+    this.learningFactsFormArray.push(learningFactForm);
+  }
+
+  removeLearningFactForm(index: number): void {
+    this.learningFactsFormArray.splice(index, 1);
+  }
+}
